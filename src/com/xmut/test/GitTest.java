944c7414d3626025da3b7e007084fd069292c27f
yh <isyanghang@163.com>
2021-08-08 14:24:11 +0800

    push commit

--- src/com/xmut/test/GitTest.java
+++ src/com/xmut/test/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("111111");
         System.out.println("222222222");
+        System.out.println("33333333");
+        System.out.println("pull test");
     }
 }
