5977d8c9e11203f93ee632812c64d388547347ca
yh <isyanghang@163.com>
2021-08-08 13:50:11 +0800

    hot-fix commit2

---
+++ src/com/xmut/test/GitTest.java
@@ -0,0 +1,12 @@
+package com.xmut.test;
+
+/**
+ * @author yhstart
+ * @create 2021-08-08 13:37
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("111111");
+        System.out.println("222222222");
+    }
+}
